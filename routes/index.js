1e5cbcd6f1dbab1d798d3ae5e177c6c146595900
Lưu Đức Hải <52000657@student.tdtu.edu.vn>
2023-04-25 10:44:35 +0700

    Home page applicant

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+    res.render('home')
+})
 router.use('/', (req, res) => {
     res.status(404).send('<h1>404 Not Found</h1>')
 })
