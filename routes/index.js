718f5bcbd3466e9fc4e0b83b610a1e5f13a1b605
Lưu Đức Hải <52000657@student.tdtu.edu.vn>
2023-04-25 11:08:51 +0700

    Layout login, register for recruiter

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('home')
 })
+router.get('/login', (req, res) => {
+    res.render('login', {layout: false})
+})
+
+router.get('/recruiter/login', (req, res) => {
+    res.render('recruiter/login', {layout: false})
+})
+
+router.get('/recruiter/register', (req, res) => {
+    res.render('recruiter/register', {layout: false})
+})
+
 router.use('/', (req, res) => {
     res.status(404).send('<h1>404 Not Found</h1>')
 })
