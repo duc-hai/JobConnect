e68699a2d78164982160d08c895ef127bc849018
Lưu Đức Hải <52000657@student.tdtu.edu.vn>
2023-04-25 15:09:59 +0700

    Complete register with recruiter

--- public/js/recruiter/register.js
+++ public/js/recruiter/register.js
@@ -3,6 +3,7 @@ const regis = document.getElementById('form-register')
 regis.addEventListener('submit', function (e) {
     e.preventDefault()
     const data = {
+        name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         password: document.getElementById('password').value,
         confirmPassword: document.getElementById('confirmPassword').value
@@ -13,5 +14,15 @@ regis.addEventListener('submit', function (e) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
+    })
+    .then(rel => rel.json())
+    .then(result => {
+        console.log(result)
+        if (result.status === 'OK') {
+            location.href = '/recruiter/login'  
+        }
+    })
+    .catch(err => {
+        console.log(err)
     })
 })
